f6b0718d679a998f77d616c8deeb7ef3323baa52
Filipe Lima <filipe.lima@arandaeditora.com.br>
2018-08-23 18:44:46 -0300

    Adicionado plugin nativo de contatos

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +8,11 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  public allContacts: any
 
+  constructor(public navCtrl: NavController, public contacts: Contacts) {
+      this.contacts.find(['name'], {filter: "", multiple: true}).then(data => {
+          this.allContacts = data
+      });
   }
-
 }
